cc598d672467f2954a1880de68158e6b1157fca0
Samuelwidjaja <james.samuel.widjaja@gmail.com>
2023-09-12 08:27:32 +0700

    message

--- main/models.py
+++ main/models.py
@@ -1,7 +1,10 @@
+# models.py in the 'main' app
 from django.db import models
 
-class Product(models.Model):
+class Item(models.Model):
     name = models.CharField(max_length=255)
-    date_added = models.DateField(auto_now_add=True)
-    price = models.IntegerField()
+    amount = models.IntegerField()
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
