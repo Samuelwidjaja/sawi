84a04af2ebc8b06bb5191b334ae5c122c961136f
Samuelwidjaja <james.samuel.widjaja@gmail.com>
2023-09-28 15:52:33 +0700

    tugas4

--- main/models.py
+++ main/models.py
@@ -1,7 +1,10 @@
 # models.py in the 'main' app
 from django.db import models
+from django.contrib.auth.models import User
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.PositiveBigIntegerField()
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from main.views import show_main, create_item, show_json, show_json_by_id, show_xml,show_xml_by_id
+from main.views import show_main, create_item, show_json, show_json_by_id, show_xml,show_xml_by_id, add_amount, reduce_amount, hapus
+from main.views import register #sesuaikan dengan nama fungsi yang dibuat
+from main.views import login_user #sesuaikan dengan nama fungsi yang dibuat
+from main.views import logout_user
 app_name = 'main'
 
 urlpatterns = [
@@ -9,4 +12,11 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('register/', register, name='register'), 
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('add_amount/<int:id>', add_amount, name='add_amount'),
+    path('reduce_amount/<int:id>', reduce_amount, name='reduce_amount'), # type: ignore
+    path('hapus/<int:id>', hapus, name='hapus'),  # type: ignore
+
 ]
